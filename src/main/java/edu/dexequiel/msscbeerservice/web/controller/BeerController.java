160e6b18bee7d2908a39e466677e0d267befc3ec
damian garcia <dexequiel.garcia@gmail.com>
2019-10-16 16:06:58 -0300

    Beer Controller initial implementation added. closes #3

---
+++ src/main/java/edu/dexequiel/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,38 @@
+package edu.dexequiel.msscbeerservice.web.controller;
+
+import edu.dexequiel.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beers")
+public class BeerController {
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<BeerDto> getById(@PathVariable("uuid") UUID uuid) {
+        // TODO impl
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody BeerDto beerDto) {
+        // TODO impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{uuid}")
+    public  ResponseEntity update(@PathVariable("uuid") UUID uuid, @RequestBody BeerDto beerDto) {
+        // TODO impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{uuid}")
+    public  ResponseEntity delete(@PathVariable("uuid") UUID uuid) {
+        // TODO impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+}
