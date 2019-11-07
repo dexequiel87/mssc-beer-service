0f3d8d982b75c00d8ed1d125c560191cc6e27275
damian garcia <dexequiel.garcia@gmail.com>
2019-11-07 10:29:59 -0300

    AsciiDoctor configuration fixed.

--- src/test/java/edu/dexequiel/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/edu/dexequiel/msscbeerservice/web/controller/BeerControllerTest.java
@@ -4,17 +4,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.dexequiel.msscbeerservice.web.model.BeerDto;
 import edu.dexequiel.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
@@ -27,8 +35,18 @@ class BeerControllerTest {
     @Test
     void getById() throws Exception {
 
-        mockMvc.perform(get("/api/v1/beers/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/api/v1/beers/{beerId}", UUID.randomUUID().toString())
+                .param("pageSize", "20")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("v1/beer/",
+                        pathParameters(
+                                parameterWithName("beerId").description("UUID of the desired beer to get")
+                        ),
+                        requestParameters(
+                                parameterWithName("pageSize").description("The page size")
+                        )
+                        ));
     }
 
     @Test
@@ -43,9 +61,9 @@ class BeerControllerTest {
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(post("/api/v1/beers")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(beerDtoJson))
-            .andExpect(status().isCreated());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isCreated());
     }
 
     @Test
